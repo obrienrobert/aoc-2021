48641669b62035aad86cb24edac7c960217d4b89
Robert O'Brien <robrien@redhat.com>
2021-12-03 15:48:20 +0000

    Initial commit

---
+++ day_1/main.py
@@ -0,0 +1,10 @@
+total_increases = 0
+
+with open('day_1/measurements.txt') as f:
+    lines = f.read().splitlines()
+
+    for index, item in enumerate(lines[0:]):
+        if int(item) > int(lines[index - 1]):
+            total_increases += 1
+
+print(total_increases)
